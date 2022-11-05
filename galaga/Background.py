c7e4cd1f2645fad162dad5351077eebf6c7d76ee
name <jamarict04@gmail.com>
2022-11-05 17:33:44 -0400

    msg

--- galaga/Background.py
+++ galaga/Background.py
@@ -2,5 +2,8 @@ import cmu_112_graphics
 
 class Background(object):
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        self.width = width #width 500
+        self.height = height #height 500
+
+    
+
