fe8d983d675566ceba4f794833f48d2946c0a8a7
name <jamarict04@gmail.com>
2022-11-05 18:03:11 -0400

    background

--- galaga/tempDraw.py
+++ galaga/tempDraw.py
@@ -10,5 +10,13 @@ class Background(object):
     def redraw(self, app, canvas):
         canvas.create_rectangle(app.width/4,0,app.width*3/4,app.height, fill = "black")
 
+def appStarted(app):
+    app.background = Background(500,500)
+
+def redrawAll(app, canvas):
+    app.background.redraw(app, canvas)
+    
 
 
+
+runApp(width = 1000,height = 500)
