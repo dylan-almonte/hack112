57c08f2548fc8e9de98774e1534d67063c1dfe1c
gsha <gsha@andrew.cmu.edu>
2022-11-05 16:52:11 -0400

    chsdhtf

--- galaga/Background.py
+++ galaga/Background.py
@@ -1,6 +1,6 @@
 import cmu_112_graphics
 
 class Background(object):
-    def __init__(self, background, something):
+    def __init__(self, background):
         self.background = background
-        self.something = something
+        
