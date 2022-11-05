3cb55c15a76f2104dd3a9b739f9671ff720861e2
name <jamarict04@gmail.com>
2022-11-05 17:57:31 -0400

    l

--- galaga/tempDraw.py
+++ galaga/tempDraw.py
@@ -1,1 +1,14 @@
 print("new")
+
+from cmu_112_graphics import *
+
+class Background(object):
+    def __init__(self, width, height):
+        self.width = width #width 500
+        self.height = height #height 500
+
+    def redraw(self, app, canvas):
+        canvas.create_rectangle(app.width/4,0,app.width*3/4,app.height, fill = "black")
+
+
+
