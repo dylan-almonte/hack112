d0025dbca4bf650c7f4ea1b8230eeb35cb77b85a
name <jamarict04@gmail.com>
2022-11-05 23:31:38 -0400

    ggg

--- galaga/main.py
+++ galaga/main.py
@@ -9,15 +9,18 @@ def appStarted(app):
     app.totalTime = 0
     app.myPlayer = Player(500, 450)
     app.enemyWave = EnemyWave()
+    app.bulletTime = 0
+    app.bulletCounter = 0
 
 
 def timerFired(app):
     app.enemyWave.spawnEnemy(app)
-
     app.background.timerFired(app)
     app.background.newStar(app)
+    app.bulletTime += 1
+    if app.bulletTime % 5 == 0:
+        app.bulletCounter = 0
     app.myPlayer.timerFired(app)
-
 
 def keyPressed(app, event):
     if event.key == "Left":
@@ -25,7 +28,9 @@ def keyPressed(app, event):
     elif event.key == "Right":
         app.myPlayer.rightMove()
     elif event.key == "Space":
-        app.myPlayer.fireBullet()
+        if app.bulletCounter < 2:
+            app.bulletCounter += 1
+            app.myPlayer.fireBullet()
 
 
 def redrawAll(app, canvas):
