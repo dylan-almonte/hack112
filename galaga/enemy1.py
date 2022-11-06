7ac00938c4efe0104993d1538db93e0f183369ca
dylan-almonte <dcabahug@andrew.cmu.edu>
2022-11-06 00:48:36 -0400

    enemy stff

--- galaga/enemy1.py
+++ galaga/enemy1.py
@@ -2,13 +2,13 @@ from cmu_112_graphics import *
 from enemy import Enemy
 
 
-class enemy1():
+class enemy1(Enemy):
     # TODO change to custom movements and timeToMove
 
-    def __init__(self, x, y):
+    def __init__(self):
         super().__init__()
-        self.x = x
-        self.y = y
+        self.x = 0
+        self.y = 0
 
         # self.moveBasic = [[1, 0]
         #                   [-1, 0]
@@ -18,6 +18,7 @@ class enemy1():
     def drawEnemy(self, canvas):
         canvas.create_oval(self.x-5, self.y-5, self.x+5, self.y+5,
                            fill='red')
+
     # def appStarted(app):
     #     app.hitBox = 5 #Change hitbox depending on enemy later //TODO
     #     app.enemyImage = app.loadImage('enemy_bumblebee.png')
@@ -25,12 +26,12 @@ class enemy1():
     # def enemyImage(app):
     #     return ImageTk.PhotoImage(app.enemyImage)
 
-    def getVel(self, time):
-        if time % self.timeToMove == 1:
-            return self.moveBasic[0]
+    # def getVel(self, time):
+    #     if time % self.timeToMove == 1:
+    #         return self.moveBasic[0]
 
-        if time % self.timeToMove == 2:
-            return self.moveBasic[1]
+    #     if time % self.timeToMove == 2:
+    #         return self.moveBasic[1]
 
-        if time % self.timeToMove == 3:
-            return self.moveBasic[2]
+    #     if time % self.timeToMove == 3:
+    #         return self.moveBasic[2]
